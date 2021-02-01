af8ee07e9d6286a8424d144d5ea2b44b0a29256b
xinghl <xinghl@chempower.com>
2021-02-01 16:52:09 +0800

    ''

---
+++ seata-order-service2001/src/main/java/com/xinghl/springcloud/alibaba/dao/OrderDao.java
@@ -0,0 +1,15 @@
+package com.xinghl.springcloud.alibaba.dao;
+
+import com.xinghl.springcloud.alibaba.domain.Order;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+@Mapper
+public interface OrderDao
+{
+    //新建订单
+    void create(Order order);
+
+    //修改订单状态，从零改为1
+    void update(@Param("userId") Long userId,@Param("status") Integer status);
+}
---
+++ seata-order-service2001/src/main/java/com/xinghl/springcloud/alibaba/service/OrderService.java
@@ -0,0 +1,9 @@
+package com.xinghl.springcloud.alibaba.service;
+
+import com.xinghl.springcloud.alibaba.domain.Order;
+
+
+public interface OrderService{
+    void create(Order order);
+}
+
